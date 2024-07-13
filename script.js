7aa035fb41b4f85895a621017a3d8bc4fdfdb7b3
Roya Arkhmammadova <151827017+RoyArkh@users.noreply.github.com>
2024-07-13 20:37:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ fetch('https://api.quotable.io/quotes/random')
     const quote = data[0].content;
     const author = data[0].author;
 
-    // Create paragraph elements
     const quoteParagraph = document.createElement('p');
     const authorParagraph = document.createElement('p');
 
-    // Set the text content of the paragraphs
     quoteParagraph.textContent = `"${quote}"`;
     authorParagraph.textContent = `- ${author}`;
 
-    // Append the paragraphs to the body
     document.body.appendChild(quoteParagraph);
     document.body.appendChild(authorParagraph);
   })
